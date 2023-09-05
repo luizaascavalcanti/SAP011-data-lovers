66e7111596313fb3aa2d2f2a537152e123e8469d
luizaascavalcanti <luiza.ascavalcanti@gmail.com>
2023-09-05 19:32:35 -0300

    update1

--- src/data.js
+++ src/data.js
@@ -1,9 +1,43 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+//Filters: name, specie, gender, origin, location, episode
+ export const nameFilter = (data, filter) => {
+    if (filter === "All") {
+      return data;
+    } else {
+      return data.filter((character) => character.name === filter);
+    }
+  };
+  export const specieFilter = (data, filter) => {
+    if (filter === "All") {
+      return data;
+    } else {
+      return data.filter((character) => character.specieFilter === filter);
+    }
+  };
+  export const genderFilter = (data, filter) => {
+    if (filter === "All") {
+      return data;
+    } else {
+      return data.filter((character) => character.genderFilterFilter === filter);
+    }
+  };
+  export const originFilter = (data, filter) => {
+    if (filter === "All") {
+      return data;
+    } else {
+      return data.filter((character) => character.originFilterFilter === filter);
+    }
+  };
+  export const locationFilter = (data, filter) => {
+    if (filter === "All") {
+      return data;
+    } else {
+      return data.filter((character) => character.locationFilter === filter);
+    }
+  };
+  export const episodeFilter = (data, filter) => {
+    if (filter === "All") {
+      return data;
+    } else {
+      return data.filter((character) => character.episodeFilter === filter);
+    }
+  };
