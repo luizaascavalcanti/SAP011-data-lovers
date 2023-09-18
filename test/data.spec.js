5bc4502c44894d95424ad6208f7af91fa03b9715
Thayjanciauskas <tatajanci@hotmail.com>
2023-09-18 19:46:52 -0300

    atualização 18.09 - teste

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,6 +1,33 @@
 import { example, anotherExample } from '../src/data.js';
+import {generalFilter} from '../src/data.js';
 
+const rick = {
+  "name": "Rick Sanchez",
+  "status": "Alive",
+  "gender": "Male",
+}
 
+const data = ["rick Sanchez"];
+
+describe('Testes da função filterByName', () => {
+
+  it('Validando que é uma função', () => {
+    expect(typeof generalFilter).toBe('function');
+  });
+
+  it('Validando que filtro por nome, filtra corretamente', () => {
+    const resultado = filterByName(data, "Rick Sanchez");
+
+    expect(resultado).toStrictEqual([rick]);
+  });
+
+  it('Validando que filtro por nome, retorna vazio, caso nome não seja encontrado', () => {
+    const resultado = filterByName(data, "batatinha");
+
+    expect(resultado).toStrictEqual([]);
+  });
+});
+//////////////////////////////////////////////////////////////
 describe('example', () => {
   it('is a function', () => {
     expect(typeof example).toBe('function');
